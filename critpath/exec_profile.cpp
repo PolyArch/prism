957e3ecec11efd56ff2223601b64da74ce8dc084
Venkatraman Govindaraju <venkatra@cs.wisc.edu>
2013-10-11 13:14:39 -0500

    added exec_profiler

---
+++ critpath/exec_profile.cpp
@@ -0,0 +1,56 @@
+
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+
+namespace {
+  class exec_profile {
+  public:
+    bool _initialized = false;
+    void init();
+    std::map<std::pair<uint64_t, int>, std::string> disasmMap;
+  };
+}
+
+static exec_profile _prof_instance;
+
+
+void exec_profile::init()
+{
+  if (_initialized)
+    return ;
+  _initialized = true;
+
+  const char *prof_fname = getenv("EXEC_PROFILE");
+  if (!prof_fname)
+    return;
+
+  std::ifstream ifs;
+  ifs.open(prof_fname);
+  std::string str;
+  while (ifs.good()) {
+    uint64_t pc;
+    int upc;
+    std::string disasm;
+    ifs >> pc;
+    ifs >> upc;
+    getline(ifs, disasm);
+    std::pair<uint64_t, int> pc_upc = std::make_pair(pc, upc);
+    disasmMap[pc_upc] = disasm;
+  }
+}
+
+namespace ExecProfile {
+  std::string _getDisasmInternal(uint64_t pc, int upc);
+  std::string _getDisasmInternal(uint64_t pc, int upc)
+  {
+    _prof_instance.init();
+
+    std::pair<uint64_t, int> pc_upc = std::make_pair(pc, upc);
+    return _prof_instance.disasmMap[pc_upc];
+  }
+}
